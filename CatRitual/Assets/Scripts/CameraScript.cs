778937f176878aa2571ad6353799697b0fce7f7e
Basher207 <basher207@gmail.com>
2016-01-29 23:23:26 +0000

    Added camera script, snaps on reaching edge

---
+++ CatRitual/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	[SerializeField] public Transform player;
+
+	void Update () {
+		if (!player) 
+			return;
+		float width = Camera.main.orthographicSize / (float)Screen.height * Screen.width;
+		Vector2 delta = player.transform.position - transform.position;
+
+		Vector3 newPos = transform.position;
+		bool changed = true;
+
+		if ((width - Mathf.Abs (delta.x)) < 0)
+			newPos.x += width * (delta.x < 0 ? -1 : 1) * 2;
+		if ((Camera.main.orthographicSize - Mathf.Abs (delta.y)) < 0)
+			newPos.y += Camera.main.orthographicSize * 2 * (delta.y < 0 ? -1 : 1);
+
+		transform.position = newPos;
+	}
+}
