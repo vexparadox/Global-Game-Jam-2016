a8f06ba1aa41062684386ac9e763049b9a1eda7d
William Meaton <will.meaton@gmail.com>
2016-01-29 20:43:45 +0000

    Started ZoneChange script

---
+++ CatRitual/Assets/ZoneChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneChange : MonoBehaviour {
+	public GameObject changeLocation;
+	public Camera cameraToChange;
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag != "Player") {
+			return;
+		}
+		cameraToChange.transform.position = changeLocation.gameObject.transform.position;
+	}
+}
