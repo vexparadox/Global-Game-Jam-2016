73bd5d67f68c9d473a732858ce6e3ca0de843d3e
William Meaton <will.meaton@gmail.com>
2016-01-30 21:26:03 +0000

    added dooruvsetter

---
+++ CatRitual/Assets/Scripts/DoorUVsSetter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class DoorUVsSetter : MonoBehaviour {
+	public MeshFilter quad;
+	public ZoneChange teleportScript;
+	// Use this for initialization
+	void Start () {
+		if (Application.isPlaying) {
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
