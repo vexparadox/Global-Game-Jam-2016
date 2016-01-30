a8abe238380664156430fc56b155acad17bdc50a
Basher207 <basher207@gmail.com>
2016-01-30 00:24:31 +0000

    Added bubble displaying

---
+++ CatRitual/Assets/Scripts/Main/GUI/BubbleManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Game.Bubble {
+	public class BubbleManager : MonoBehaviour {
+	
+		[SerializeField] public float timeBetweenBubbles = 0.4f;
+	
+		[HideInInspector] public int indexOfCurrentBubble = 0;
+		[HideInInspector] public float timeOfLastBubbleCreation;
+	
+		void Start () {
+			timeOfLastBubbleCreation = Time.time;
+		}
+	
+		void Update () {
+			if (Time.time > timeOfLastBubbleCreation + timeBetweenBubbles) {
+				if (indexOfCurrentBubble < transform.childCount)
+					transform.GetChild (indexOfCurrentBubble++).gameObject.SetActive (true);
+				timeOfLastBubbleCreation = Time.time;
+			}
+		}
+	}
+}
---
+++ CatRitual/Assets/Scripts/Main/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static UnityEvent onDayUpdate;
+
+	public static int day = 0;
+
+	public static void AddAction (UnityAction action) {
+		if (onDayUpdate == null)
+			onDayUpdate = new UnityEvent ();
+		onDayUpdate.AddListener (action);
+	}
+	
+}
